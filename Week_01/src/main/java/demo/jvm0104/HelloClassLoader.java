f2bda05e92b62111c8e054689362717b52bd4c4c
zqlu <luzengqing@126.com>
2020-10-17 17:50:23 +0800

    使用ByteArrayOutputStream方法写字节

--- Week_01/src/main/java/demo/jvm0104/HelloClassLoader.java
+++ Week_01/src/main/java/demo/jvm0104/HelloClassLoader.java
@@ -1,5 +1,6 @@
 package demo.jvm0104;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.lang.reflect.Method;
@@ -8,13 +9,15 @@ public class HelloClassLoader extends ClassLoader {
 
     public static void main(String[] args) throws Exception {
         File file = new File("/Users/zqlu/MyProject/JAVA-000/Week_01/src/main/java/demo/jvm0104/Hello.xlass");
-        byte[] bytes = new byte[(int) file.length()];
 
-        new FileInputStream(file).read(bytes);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        FileInputStream fileInputStream = new FileInputStream(file);
 
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte)(255 - bytes[i]);
+        int b = 0;
+        while ((b = fileInputStream.read()) != -1) {
+            byteArrayOutputStream.write(255-b);
         }
+        byte[] bytes = byteArrayOutputStream.toByteArray();
 
         Class<?> aClass = new HelloClassLoader().defineClass("Hello", bytes, 0, bytes.length);
         Object obj = aClass.newInstance();
